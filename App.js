eb5adc9bbb621f766b6016e1706d0b92188cd7ee
Kerde123 <kerttu.vanhala@gmail.com>
2023-01-17 19:33:50 +0200

    Täydennetty

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState('')
+
+  const pressed = () => {
+    Alert.alert("Kirjoitit " + text)
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your Harjoitusprojekti!</Text>
+      <TextInput
+        style={{ width: 200, borderColor: 'black', borderWidth: 2}}
+        onChangeText={text => setText(text)}
+        value= {text}
+        />
+
+      <Button title="Paina" onPress={pressed} />
       <StatusBar style="auto" />
     </View>
   );
